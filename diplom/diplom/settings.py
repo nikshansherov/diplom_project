8e8d295d0eda6982548ce282fd473cd6379b1eb1
nikshansherov <nikshansherov@yandex.ru>
2023-12-22 00:17:35 +0300

    end

--- diplom/diplom/settings.py
+++ diplom/diplom/settings.py
@@ -20,15 +20,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4(3jtw#m+%$7weu057l$53s5b0@_g!(^a2&432h7gqa_259d*1'
+import os
+
+SECRET_KEY = 'SECRET_KEY'
+SESSION_COOKIE_SECURE =True
+CSRF_COOKIE_SECURE =True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'nikshgb.pythonanywhere.com',
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR, 'media/'
 
 CART_SESSION_ID = 'cart'
 
@@ -83,8 +89,15 @@ WSGI_APPLICATION = 'diplom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nikshgb$default',
+        'USER': 'nikshgb',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'nikshgb.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -124,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
